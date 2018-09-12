3664a2380e3c16ccac78be6c3902c7d914cceefc
Liam-Cafearo <lcafearo@gmail.com>
2018-09-12 16:44:03 +0100

    corrected edit url

--- reusable_blog/urls.py
+++ reusable_blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^/$', views.post_list, name="post_list"),
     url(r'^/stuff/$', views.post_list, name="post_list"),
     url(r'^(?P<id>\d+)/$', views.post_detail),
+    url(r'^blog/(?P<id>\d+)/edit$', views.edit_post, name='edit')
     url(r'^post/$', views.new_post, name='new_post'),
     
 ]
